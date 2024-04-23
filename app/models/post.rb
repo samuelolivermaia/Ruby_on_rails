c83f1b637be85b76752a24526f0819f7b37efe4f
Samuel Oliveira <samuelmaia1020@gmail.com>
2024-04-23 17:16:24 -0300

    Atualizações

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+    validates :title, :description, presence: true 
+    validates :title, length: {minimum: 5 }
+    validates :description, length: {minimum: 15 }
+
+    before_save do 
+        self.title = self.title + " Samuel"
+    end
 end
